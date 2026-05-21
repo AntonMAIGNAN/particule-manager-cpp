d88561a3d417308027b7ac7ee150a0a49c4a9128
anton.maignan <anton.maignan@emeria.eu>
2026-05-21 10:29:58 +0200

    feat: add ExplosionConfig and ParticlePrototype classes

---
+++ ParticlePrototype.cpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+// =================================================================================
+// IParticlePrototype 
+// interface de clonage pour les particules.
+// =================================================================================
+class IParticlePrototype
+{
+public:
+    virtual IParticlePrototype* clone() const = 0;
+
+    virtual ~IParticlePrototype() {}
+};
+
+// =================================================================================
+// ParticlePrototype
+// Particule concrète représentant une particule
+// =================================================================================
+class ParticlePrototype : public IParticlePrototype
+{
+private:
+    float size;
+    string color;
+    float speed;
+    float x, y;
+    string texture;
+    string shader;
+    string physics;
+
+public:
+    ParticlePrototype(float size, const string &color, float speed, float x, float y, const string &texture, const string &shader, const string &physics) 
+    {
+        this->size = size;
+        this->color = color;
+        this->speed = speed;
+        this->x = x;
+        this->y = y;
+        this->texture = texture;
+        this->shader = shader;
+        this->physics = physics;
+    }
+
+    IParticlePrototype* clone() const override
+    {
+        return new ParticlePrototype(*this);
+    }
+};
