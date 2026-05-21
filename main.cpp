1a1df8d634ecff4abbe2f8a4dc39e99ba23066a9
anton.maignan <anton.maignan@emeria.eu>
2026-05-21 12:49:27 +0200

    feat: implement ExplosionFacade for managing explosion creation and rendering

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 #include "Explosion.cpp"
 #include "ExplosionFactory.cpp"
 #include "ExplosionCacheProxy.cpp"
+#include "ExplosionFacade.cpp"
 
 using namespace std;
 
 int main() 
 {
-    ExplosionCacheProxy proxy;
     ExplosionBuilder builder;
-    ExplosionFactory factory;
     
     shared_ptr<ExplosionConfig> config1 = builder
         .withPosition(10, 10)
@@ -18,8 +17,6 @@ int main()
         .withCount(5)
         .withSpread(10.0f)
         .build();
-    Explosion exp1 = proxy.create(*config1);
-    exp1.render();
     
     shared_ptr<ExplosionConfig> config2 = builder
         .withPosition(10, 10)
@@ -27,17 +24,24 @@ int main()
         .withCount(5)
         .withSpread(10.0f)
         .build();
-    Explosion exp2 = proxy.create(*config2);
-    exp2.render();
-    
+
+
     shared_ptr<ExplosionConfig> config3 = builder
         .withPosition(20, 20)
         .withColor("yellow")
         .withCount(10)
         .withSpread(10.0f)
         .build();
-    Explosion exp3 = factory.create(*config3);
-    exp3.render();
+
+    ExplosionFacade::getInstance()
+        ->triggerExplosion(*config1);
+
+    ExplosionFacade::getInstance()
+        ->triggerExplosion(*config2);
+
+    ExplosionFacade::getInstance()
+        ->triggerExplosionWithoutProxy(*config3);
 
     return 0;
 }
+
